cd053084b96846b34c3139fc29ae72e5766e6223
Priya17727 <y.sivapriya2005@gmail.com>
2026-06-05 12:11:31 +0530

    Update app.js

--- q1/app.js
+++ q1/app.js
@@ -1,9 +1,9 @@
 const axios = require("axios");
 
-// 🔴 PASTE YOUR TOKEN HERE (INSIDE QUOTES)
+
 const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJNYXBDbGFpbXMiOnsiYXVkIjoiaHR0cDovLzIwLjI0NC41Ni4xNDQvZXZhbHVhdGlvbi1zZXJ2aWNlIiwiZW1haWwiOiIyM2JxMWE1NGY4QHZ2aXQubmV0IiwiZXhwIjoxNzgwNjM3Mjg2LCJpYXQiOjE3ODA2MzYzODYsImlzcyI6IkFmZm9yZCBNZWRpY2FsIFRlY2hub2xvZ2llcyBQcml2YXRlIExpbWl0ZWQiLCJqdGkiOiIxMGFhZTU2Yy00YWVjLTQ5NzgtOWVkZC03ZDM0MGNmN2JiMDciLCJsb2NhbGUiOiJlbi1JTiIsIm5hbWUiOiJzaXZhIHByaXlhIHlla3VsYSIsInN1YiI6IjRjOWUxODFkLTdhYTItNDQ4Yi05ODIxLWUxYzQ0NWRlYjJiYyJ9LCJlbWFpbCI6IjIzYnExYTU0ZjhAdnZpdC5uZXQiLCJuYW1lIjoic2l2YSBwcml5YSB5ZWt1bGEiLCJyb2xsTm8iOiIyM2JxMWE1NGY4IiwiYWNjZXNzQ29kZSI6IlFRZEVZeSIsImNsaWVudElEIjoiNGM5ZTE4MWQtN2FhMi00NDhiLTk4MjEtZTFjNDQ1ZGViMmJjIiwiY2xpZW50U2VjcmV0IjoiQ3pudlVGR25hclVtQXJYVCJ9.nVEp1IeD6UqdT6bsn-8BZQBGBtgJDFkt0Bbp-qsHZjA";
 
-// 🔴 API URL
+
 const BASE_URL = "http://20.244.56.144/evaluation-service";
 
 async function getNotifications() {
@@ -12,7 +12,7 @@ async function getNotifications() {
 
     const response = await axios.get(`${BASE_URL}/notifications`, {
       headers: {
-        Authorization: `Bearer ${token}` // ✅ correct format
+        Authorization: `Bearer ${token}` 
       }
     });
 
@@ -29,5 +29,4 @@ async function getNotifications() {
   }
 }
 
-// run function
 getNotifications();
